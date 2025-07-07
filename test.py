b51ebd3b96ec29ae44ce3c59e99e4e9b6c99cc73
Marc <gdleds31@gmail.com>
2025-07-07 15:49:59 +0000

    premier push

---
+++ test.py
@@ -0,0 +1,41 @@
+import pytest
+import pandas as pd
+from etl import extract_data, transform_data, load_data
+
+
+
+
+def test_extract_data():
+    #création d'un fichier temporaire csv
+    with tempfile.NamedTemporaryFile(mode='w', suffix='.csv', delete=False) as tmp:
+        tmp.write("col1,col2\n1,2\n3,4\n")
+        tmp_path = tmp.name
+    df = extract_data(tmp_path)
+    # Nettoyage du fichier temporaire
+    os.remove(tmp_path)
+    # test de ce fichier
+    assert df is not None
+    assert isinstance(df, pd.DataFrame)
+    # Test du message d'erreur
+    df = extract_data("non_existent_file.csv")
+    assert df is None
+
+
+def test_transform_data():
+    df = pd.DataFrame({
+        'name': ['Alice', 'Bob', 'Charlie'],
+        'salary': [5000, 6000, 7000]
+    })
+
+    result = transform_data(df)
+
+    # Vérifications
+    assert result is not None
+    assert 'tax' in result.columns
+    assert 'net_salary' in result.columns
+    assert result.shape[0] == 3
+    assert result['tax'].iloc[0] == 500.0
+    assert result['net_salary'].iloc[1] == 5400.0
+
+def test_load_data():
+    
