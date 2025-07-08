2133adc32eaa7d2019f7f8510931171d0803907f
Marc <gdleds31@gmail.com>
2025-07-08 16:18:25 +0200

    Add files via upload

--- tests/upload_s3.py
+++ tests/upload_s3.py
@@ -7,13 +7,13 @@ s3_client = boto3.client("s3")
 # file_name = "unit-tests.xml"  # Nom du fichier XML généré par pytest
 file_name = "results.xml"
 bucket_name = "kub8jed31"
-# s3_key = "/unit-tests.xml"  # Chemin dans le bucket
+s3_key = "/unit-tests.xml"  # Chemin dans le bucket
 
 # Upload du fichier
 try:
     s3_client.upload_file(file_name, bucket_name, s3_key)
     print(
-        f"✅ Fichier '{file_name}' envoyé avec succès sur 's3://{bucket_name}" #/{s3_key}'"
+        f"✅ Fichier '{file_name}' envoyé avec succès sur 's3://{bucket_name}/{s3_key}'"
     )
 except Exception as e:
     print(f"❌ Erreur lors de l'upload sur S3 : {e}")
